6492c400294b4e002fa6a324bd71e7328adbd5f9
Royc30ne <github@lpy.email>
2023-04-08 19:47:21 +0100

    update server structure

--- fedl/Server/base_server.py
+++ fedl/Server/base_server.py
@@ -4,22 +4,14 @@ from abc import ABC, abstractmethod
 from fedl.Server.Aggregator.aggregator import Aggregator
 from fedl.utils.model_constants import BYTES_WRITTEN_KEY, BYTES_READ_KEY, LOCAL_COMPUTATIONS_KEY
 class BaseServer(ABC):
-    def __init__(self, client_model, aggregator:Aggregator, model_params=None):
+    def __init__(self, client_model, aggregator:Aggregator):
         self.client_model = client_model
         self.aggregator = aggregator
-        if model_params is not None:
+        if client_model is not None:
             self.model = client_model.get_params()
         self.selected_clients = []
         self.adversaries = []
         self.updates = []
-
-    def set_clients(self, clients, num_clients, cur_round):
-        self.clients = clients
-        num_clients = min(num_clients, len(clients))
-        np.random.seed(cur_round + np.random.randint(10000)) 
-        self.selected_clients = np.random.choice(clients, num_clients, replace=False)
-
-        return [(c.num_train_samples, c.num_test_samples, c.id) for c in self.selected_clients]
 
     def client_train(self, single_center, num_epochs=1, batch_size=10, minibatch=None, clients=None, apply_prox=False):
         if clients is None:
@@ -45,7 +37,60 @@ class BaseServer(ABC):
 
     def aggregate(self):
         self.model = self.aggregator.aggregate(self.updates)
+        self.updates = []
         return self.model
+    
+    def get_clients_info(self, clients):
+        """Returns the ids, hierarchies and num_samples for the given clients.
+        Returns info about self.selected_clients if clients=None;
+        Args:
+            clients: list of Client objects.
+        """
+        if clients is None:
+            clients = self.selected_clients
+
+        ids = [c.id for c in clients]
+        groups = {c.id: c.group for c in clients}
+        num_samples = {c.id: c.num_samples for c in clients}
+        return ids, groups, num_samples
+    
+    def test_model(self, clients_to_test, set_to_use='test'):
+        """
+        Test model for different comparison of metrics
+        and save them to report file for plotting
+
+        """
+        metrics = {}
+
+        if clients_to_test is None:
+            clients_to_test = self.selected_clients
+
+        for client in clients_to_test:
+            client.model.set_params(self.model)
+            c_metrics = client.test(set_to_use)
+            metrics[client.id] = c_metrics
+
+        return metrics
+    
+    def select_clients(self, possible_clients, num_clients=20, my_round=100):
+        """Selects num_clients clients randomly from possible_clients.
+        
+
+        Note that within function, num_clients is set to
+            min(num_clients, len(possible_clients)).
+            
+        Args:
+            possible_clients: Clients from which the server can select.
+            num_clients: Number of clients to select; default 20
+        Return:
+            list of (num_train_samples, num_test_samples)
+        """
+        num_clients = min(num_clients, len(possible_clients))
+        np.random.seed(my_round + np.random.randint(10000)) 
+        self.selected_clients = np.random.choice(possible_clients, num_clients, replace=False)
+
+        return [(c.num_train_samples, c.num_test_samples) for c in self.selected_clients]
+    
     
     def __model__(self):
         return self.model
